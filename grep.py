68a7950edf6c675108dae3cfbc8d9b9cc1f049a1
abhitrueprogrammer <67090539+abhitrueprogrammer@users.noreply.github.com>
2024-03-02 23:51:14 +0530

    Update grep.py

--- grep.py
+++ grep.py
@@ -6,47 +6,47 @@ import re
 def preparePattern(pattern, wordOnly=False):
     #the .*{pattern}*. wrapper is required to match the default of grep that
     #checks for the substring inside any string no mattern what surrounds the pattern.
-    insideSurround = '.*'
-    outsideSurround = '.*'
+    inside_surround = '.*'
+    outside_surround = '.*'
 
     #wordboundry check
     if wordOnly:
-        insideSurround = insideSurround + r'\b'
-        outsideSurround = r'\b' + outsideSurround
+        inside_surround = inside_surround + r'\b'
+        outside_surround = r'\b' + outside_surround
 
-    return insideSurround +  pattern + outsideSurround
+    return inside_surround +  pattern + outside_surround
 arguments = sys.argv
 i = 1 # A index for where the pattern file starts
 
 #finding and seting up flags
-caseInsensitive, lineCount, invert, wordOnly = False, False, False, False
+case_insensitive, line_count, invert, word_only = False, False, False, False
 if '-i' in arguments:
-    caseInsensitive = True
+    case_insensitive = True
     i += 1
 if '-c' in arguments:
-    lineCount = True
+    line_count = True
     i += 1
 if '-v' in arguments:
     invert = True
     i += 1
 if '-w' in arguments:
-    wordOnly = True
+    word_only = True
     i += 1
 
 pattern = sys.argv[i]
-pattern = preparePattern(pattern, wordOnly)
+pattern = preparePattern(pattern, word_only)
 files = sys.argv[i+1:] #all the file names
 lc = 0 #line count variable. used lc for convention
 
 for file in files:
     with open(file, 'r') as f:
-        lineLst = f.readlines()
-        lineLst = [i.strip() for i in lineLst]
-        for line in lineLst:
+        line_lst = f.readlines()
+        line_lst = [i.strip() for i in line_lst]
+        for line in line_lst:
             result = None
             #matching the file through regex. 
 
-            if(caseInsensitive):
+            if(case_insensitive):
                 result = re.match(pattern, line, re.IGNORECASE)
             else:
                 result = re.match(pattern, line)
@@ -58,7 +58,7 @@ for file in files:
 
             if condition:
                 lc += 1
-                if not lineCount:
+                if not line_count:
                     print(line) # Won't print lines if -l option is enabled
-if (lineCount): 
+if (line_count): 
     print(lc)
