5767698af7eff3c325795bc61bc140ec9cb3a542
abhitrueprogrammer <67090539+abhitrueprogrammer@users.noreply.github.com>
2024-03-03 00:55:26 +0530

    Update grep.py

--- grep.py
+++ grep.py
@@ -1,30 +1,41 @@
-'''Usage: grep [options] pattern filename'''
+'''Usage: python3 grep [options] pattern filename'''
+'''Example: python3 grep.py -n -w clear GOL.c grep.py'''
 import sys 
 import re
-
+import os
 '''Function to take the pattern and prepare it for grep to work as expected'''
-def preparePattern(pattern, wordOnly=False):
+'''Input: pattern and optionally status of word_only'''
+'''Output: prepared string. '''
+def preparePattern(pattern, word_only=False):
     #the .*{pattern}*. wrapper is required to match the default of grep that
     #checks for the substring inside any string no mattern what surrounds the pattern.
     inside_surround = '.*'
     outside_surround = '.*'
 
-    #wordboundry check
-    if wordOnly:
+    #Adds wordboundry check if only word option is enabled
+    if word_only:
         inside_surround = inside_surround + r'\b'
         outside_surround = r'\b' + outside_surround
 
     return inside_surround +  pattern + outside_surround
+'''Check if file doesn't exist'''
+'''Input: list of files'''
+'''Return: File name that doesn't exist else 0'''
+def fileNotExist(fileList):
+    for file in fileList:
+        if not( os.path.exists(os.path.join(os.getcwd(), file))):
+            return file
+    return 0
 arguments = sys.argv
 i = 1 # A index for where the pattern file starts
 
 #finding and seting up flags
-case_insensitive, line_count, invert, word_only = False, False, False, False
+case_insensitive = show_only_matching_lc = invert = word_only = show_line_count = show_file_name = directory = False
 if '-i' in arguments:
     case_insensitive = True
     i += 1
 if '-c' in arguments:
-    line_count = True
+    show_only_matching_lc = True
     i += 1
 if '-v' in arguments:
     invert = True
@@ -32,20 +43,41 @@ if '-v' in arguments:
 if '-w' in arguments:
     word_only = True
     i += 1
+if '-n' in arguments:
+    show_line_count = True
+    i += 1
+if '-r' in arguments:
+    i+=1
+    directory = True
+    
 
 pattern = sys.argv[i]
 pattern = preparePattern(pattern, word_only)
-files = sys.argv[i+1:] #all the file names
-lc = 0 #line count variable. used lc for convention
+
+#if -r set than search the directory that's mentioned as the first argument 
+if directory:
+    files = [f"./{sys.argv[i+1]}/" + filename for filename in os.listdir(sys.argv[i+1])]
+    files = files + sys.argv[i+2:]
+else:
+    files = sys.argv[i+1:] #all the file names
+ghostFile = fileNotExist(files)
+if(ghostFile):
+    print(f"ERROR: {ghostFile} doesn't exist")
+    exit()
+#need to show filenames if multiple files are included
+if(len(files) > 1):
+    show_file_name = True
+found_instances_n = 0 #line count variable. used lc for convention
 
 for file in files:
     with open(file, 'r') as f:
+        lc = 0 #short for line count. Following the convention ;)
         line_lst = f.readlines()
         line_lst = [i.strip() for i in line_lst]
         for line in line_lst:
-            result = None
+            lc += 1
             #matching the file through regex. 
-
+            
             if(case_insensitive):
                 result = re.match(pattern, line, re.IGNORECASE)
             else:
@@ -57,8 +89,14 @@ for file in files:
                 condition = result is not None
 
             if condition:
-                lc += 1
-                if not line_count:
-                    print(line) # Won't print lines if -l option is enabled
-if (line_count): 
-    print(lc)
+                found_instances_n += 1
+                if not show_only_matching_lc: # Won't print lines if -c option is enabled
+                    if(show_file_name):
+                        print(file, end=":")
+                    if(show_line_count):
+                        print(lc, end=":")
+                    
+                    print(line) 
+
+if (show_only_matching_lc): 
+    print(found_instances_n)
